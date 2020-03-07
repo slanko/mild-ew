48b81597fbc9f8df564b00d46fec2c9d3ba74a23
Unknown <QMBFXSL@gmail.com>
2020-03-07 22:10:25 +1300

    gayme
    hua haaha

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float lerpTime;
+    public float camLerpTime;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, lerpTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, followTarget.transform.rotation, camLerpTime);
+    }
+
+
+}
---
+++ Assets/controllerInputz.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllerInputz : MonoBehaviour
+{
+    public float moveMult;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * moveMult);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        if(Input.GetButton("LB"))
+        {
+            transform.Rotate(0, -3, 0);
+        }
+        if (Input.GetButton("RB"))
+        {
+            transform.Rotate(0, 3, 0);
+        }
+    }
+}
